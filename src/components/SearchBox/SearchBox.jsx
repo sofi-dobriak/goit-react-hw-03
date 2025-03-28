26eb4b399b846fcf0e8b45dc6232fe448f6393c0
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-03-28 16:40:27 +0200

    fix styles

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,4 +1,3 @@
-import { Field, Form, Formik } from 'formik';
 import s from './SearchBox.module.css';
 import { useId } from 'react';
 
@@ -7,14 +6,10 @@ const SearchBox = () => {
 
     return (
         <section className={s.searchFormSection}>
-            <Formik>
-                <Form className={s.form}>
-                    <label htmlFor={findInputId} className={s.label}>
-                        Find contacts by name
-                    </label>
-                    <Field className={s.input} id={findInputId}></Field>
-                </Form>
-            </Formik>
+            <label htmlFor={findInputId} className={s.label}>
+                Find contacts by name
+            </label>
+            <input className={s.input} id={findInputId}></input>
         </section>
     );
 };
